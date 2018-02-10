fb0560c0d1d908e69c1d81a29bea859b77f51639
Nico Stapelbroek <nstapelbroek@gmail.com>
2018-02-10 22:28:02 +0100

    Add tests for the dummy adapter

--- handlers/gate_test.go
+++ handlers/gate_test.go
@@ -1,13 +1,13 @@
 package handlers
 
 import (
+	"context"
+	"github.com/nstapelbroek/gatekeeper/adapters/dummy"
+	"github.com/nstapelbroek/gatekeeper/middlewares"
+	"net"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	"context"
-	"github.com/nstapelbroek/gatekeeper/middlewares"
-	"net"
-	"github.com/nstapelbroek/gatekeeper/adapters/dummy"
 )
 
 func prepareRequest(t *testing.T) *http.Request {
@@ -26,20 +26,18 @@ func prepareRequest(t *testing.T) *http.Request {
 func TestGateOpenHandler(t *testing.T) {
 	req := prepareRequest(t)
 	adapterInstance := dummy.NewDummyAdapter()
-	gateHandler := NewGateHandler(adapterInstance, 1, )
+	gateHandler := NewGateHandler(adapterInstance, 1)
 
 	rr := httptest.NewRecorder()
 	handler := http.HandlerFunc(gateHandler.PostOpen)
 	handler.ServeHTTP(rr, req)
 
 	if status := rr.Code; status != http.StatusCreated {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusCreated)
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusCreated)
 	}
 
 	expected := `127.0.0.1 has been whitelisted for 120 seconds`
 	if rr.Body.String() != expected {
-		t.Errorf("handler returned unexpected body: got %v want %v",
-			rr.Body.String(), expected)
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
 	}
 }
