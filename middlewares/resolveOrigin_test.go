c1b1b8cf711a9075cecd441261dcf492060d9268
Nico Stapelbroek <nstapelbroek@gmail.com>
2018-02-10 19:46:15 +0100

    Add tests for the post body and http headers resolver

--- middlewares/resolveOrigin_test.go
+++ middlewares/resolveOrigin_test.go
@@ -1,6 +1,7 @@
 package middlewares
 
 import (
+	"bytes"
 	"github.com/Sirupsen/logrus"
 	"github.com/spf13/viper"
 	"io/ioutil"
@@ -16,11 +17,110 @@ func getTestSubjectResolveOrigin(resolveType string) func(handler http.Handler) 
 
 	c := viper.New()
 	c.SetDefault("resolve_type", resolveType)
+	c.SetDefault("resolve_header", "X-Forwarded-For")
 
 	return ResolveOrigin(c)
 }
 
-func TestResolveFailedReturnsAServerError(t *testing.T) {
+const wantedIP = "123.123.123.123"
+
+// Happy paths
+func getHappyPathHandlerFunc(t *testing.T) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		contextOrigin := req.Context().Value(OriginContextKey)
+		ip, assertionSucceeded := contextOrigin.(net.IP)
+		if !assertionSucceeded {
+			t.Errorf("Response context was not an instance of net.IP")
+		}
+
+		if ip.String() != wantedIP {
+			t.Errorf("resolved IP was malformed got %v wanted %v", ip.String(), wantedIP)
+		}
+	})
+}
+
+func TestResolveOriginWithDefaultResolver(t *testing.T) {
+	req, err := http.NewRequest(http.MethodPost, "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req.RemoteAddr = wantedIP + ":12345"
+	testHandler := getHappyPathHandlerFunc(t)
+
+	middleWare := getTestSubjectResolveOrigin("default")
+	rr := httptest.NewRecorder()
+	handler := middleWare(testHandler)
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+}
+
+func TestResolveOriginWithHeaderResolver(t *testing.T) {
+	req, err := http.NewRequest(http.MethodPost, "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req.Header.Add("X-Forwarded-For", wantedIP)
+	testHandler := getHappyPathHandlerFunc(t)
+
+	middleWare := getTestSubjectResolveOrigin("headers")
+	rr := httptest.NewRecorder()
+	handler := middleWare(testHandler)
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+}
+
+func TestResolveOriginWithCustomHeaderResolver(t *testing.T) {
+	req, err := http.NewRequest(http.MethodPost, "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req.Header.Add("X-Real-IP", wantedIP)
+	testHandler := getHappyPathHandlerFunc(t)
+
+	logrus.SetOutput(ioutil.Discard)
+	c := viper.New()
+	c.SetDefault("resolve_type", "headers")
+	c.SetDefault("resolve_header", "X-Real-IP")
+
+	middleWare := ResolveOrigin(c)
+	rr := httptest.NewRecorder()
+	handler := middleWare(testHandler)
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+}
+
+func TestResolveOriginWithBodyResolver(t *testing.T) {
+	req, err := http.NewRequest(http.MethodPost, "/", bytes.NewBuffer([]byte(wantedIP)))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	testHandler := getHappyPathHandlerFunc(t)
+
+	middleWare := getTestSubjectResolveOrigin("body")
+	rr := httptest.NewRecorder()
+	handler := middleWare(testHandler)
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+}
+
+// Exceptionals
+func TestNoRemoteAddrWhenUsingDefaultResolver(t *testing.T) {
 	req, err := http.NewRequest(http.MethodPost, "/", nil)
 	if err != nil {
 		t.Fatal(err)
@@ -40,29 +140,23 @@ func TestResolveFailedReturnsAServerError(t *testing.T) {
 	}
 }
 
-func TestResolveOriginWithDefaultResolver(t *testing.T) {
-	req, err := http.NewRequest(http.MethodPost, "/", nil)
+func TestInvalidBodyUsingTheBodyResolver(t *testing.T) {
+	req, err := http.NewRequest(http.MethodPost, "/", bytes.NewBuffer([]byte("invalid ip")))
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	req.RemoteAddr = "123.123.123.123:12345"
-
-	testHandler := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		contextOrigin := req.Context().Value(OriginContextKey)
-		_, assertionSucceeded := contextOrigin.(net.IP)
-		if !assertionSucceeded {
-			t.Errorf("Response context was not an instance of net.IP")
-		}
+	testHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		t.Errorf("Handler was called while the request should have been terminated")
 	})
 
-	middleWare := getTestSubjectResolveOrigin("default")
+	middleWare := getTestSubjectResolveOrigin("body")
 	rr := httptest.NewRecorder()
 	handler := middleWare(testHandler)
 	handler.ServeHTTP(rr, req)
 
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	if status := rr.Code; status != http.StatusInternalServerError {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusInternalServerError)
 	}
 }
 
