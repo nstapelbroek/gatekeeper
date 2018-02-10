06b5b1b4798bbc1e8fceb423631fc8462d7a1449
Nico Stapelbroek <nstapelbroek@gmail.com>
2018-02-10 21:35:10 +0100

    Add domain tests

---
+++ domain/firewall/direction_test.go
@@ -0,0 +1,48 @@
+package firewall
+
+import (
+	"testing"
+)
+
+func testNewDirectionFromString(testValue string, t *testing.T) {
+	direction, _ := NewDirectionFromString(testValue)
+	if direction.String() != testValue {
+		t.Errorf("Direction returned the wrong string value, got %v want %v", direction.String(), testValue)
+	}
+}
+
+func TestNewDirectionFromStringInbound(t *testing.T) {
+	testNewDirectionFromString("inbound", t)
+}
+
+func TestNewDirectionFromStringOutbound(t *testing.T) {
+	testNewDirectionFromString("outbound", t)
+}
+
+func TestNewDirectionFromStringGracefullyHandlesCapitalisation(t *testing.T) {
+	direction, _ := NewDirectionFromString("INBounD")
+	if direction.String() != "inbound" {
+		t.Errorf("Direction returned the wrong string value, got %v want %v", direction.String(), "inbound")
+	}
+}
+
+func TestNewDirectionFromInvalidString(t *testing.T) {
+	_, err := NewDirectionFromString("This_value_sucks")
+	if err != ErrInvalidDirectionString {
+		t.Errorf("Direction constructor did not return the expected error")
+	}
+}
+
+func TestDirection_IsInbound(t *testing.T) {
+	direction, _ := NewDirectionFromString("INBOUND")
+	if !direction.IsInbound() {
+		t.Errorf("Direction failed to identify itself as inbound")
+	}
+}
+
+func TestDirection_IsOutbound(t *testing.T) {
+	direction, _ := NewDirectionFromString("OUTBOUND")
+	if !direction.IsOutbound() {
+		t.Errorf("Direction failed to identify itself as outbound")
+	}
+}
