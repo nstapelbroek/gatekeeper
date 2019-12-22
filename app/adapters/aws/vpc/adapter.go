6e34622fc76618b2b1ac7856d431a0cd31e8be40
Nico Stapelbroek <nstapelbroek@gmail.com>
2019-12-22 12:01:47 +0100

    Implement EC2 ACL entry removal

--- app/adapters/aws/vpc/adapter.go
+++ app/adapters/aws/vpc/adapter.go
@@ -8,20 +8,22 @@ import (
 )
 
 type adapter struct {
-	client          *ec2.Client
-	NetworkAclId    string
-	startRuleNumber int64
-	ruleStepsTaken  int
-	ruleStepSize    int
+	client           *ec2.Client
+	NetworkAclId     string
+	startRuleNumber  int64
+	ruleStepsTaken   int
+	ruleStepSize     int
+	ruleNumbersIndex map[string]int64
 }
 
 func NewAWSNetworkACLAdapter(client *ec2.Client, networkAclId string, startRuleNumber int64) *adapter {
 	return &adapter{
-		client:          client,
-		NetworkAclId:    networkAclId,
-		startRuleNumber: startRuleNumber,
-		ruleStepsTaken:  0,
-		ruleStepSize:    10,
+		client:           client,
+		NetworkAclId:     networkAclId,
+		startRuleNumber:  startRuleNumber,
+		ruleStepsTaken:   0,
+		ruleStepSize:     10,
+		ruleNumbersIndex: make(map[string]int64),
 	}
 }
 
@@ -51,33 +53,53 @@ func (a *adapter) getProtocolNumber(protocol domain.Protocol) *string {
 	return aws.String("-1")
 }
 
+func (a *adapter) buildCreateAclEntryRequest(rule domain.Rule) *ec2.CreateNetworkAclEntryInput {
+	input := ec2.CreateNetworkAclEntryInput{
+		Egress:       aws.Bool(rule.Direction.IsOutbound()),
+		NetworkAclId: aws.String(a.NetworkAclId),
+		PortRange:    &ec2.PortRange{From: aws.Int64(int64(rule.Port.BeginPort)), To: aws.Int64(int64(rule.Port.EndPort))},
+		Protocol:     a.getProtocolNumber(rule.Protocol),
+		RuleAction:   "allow",
+		RuleNumber:   a.getNextRuleNumber(),
+	}
+
+	if rule.IPNet.IP.To4() == nil {
+		input.Ipv6CidrBlock = aws.String(rule.IPNet.String())
+	} else {
+		input.CidrBlock = aws.String(rule.IPNet.String())
+	}
+
+	return &input
+}
+
 func (a *adapter) CreateRules(rules []domain.Rule) (result domain.AdapterResult) {
 	for _, rule := range rules {
-		input := ec2.CreateNetworkAclEntryInput{
-			Egress:       aws.Bool(rule.Direction.IsOutbound()),
-			NetworkAclId: aws.String(a.NetworkAclId),
-			PortRange:    &ec2.PortRange{From: aws.Int64(int64(rule.Port.BeginPort)), To: aws.Int64(int64(rule.Port.EndPort))},
-			Protocol:     a.getProtocolNumber(rule.Protocol),
-			RuleAction:   "allow",
-			RuleNumber:   a.getNextRuleNumber(),
-		}
-
-		if rule.IPNet.IP.To4() == nil {
-			input.Ipv6CidrBlock = aws.String(rule.IPNet.String())
-		} else {
-			input.CidrBlock = aws.String(rule.IPNet.String())
-		}
-
-		req := a.client.CreateNetworkAclEntryRequest(&input)
-		_, err := req.Send(context.TODO())
-		if err != nil {
-			return domain.AdapterResult{Error: err}
-		}
+		input := a.buildCreateAclEntryRequest(rule)
+		req := a.client.CreateNetworkAclEntryRequest(input)
+		_, _ = req.Send(context.TODO()) // TODO error handling
+		a.ruleNumbersIndex[rule.String()] = *input.RuleNumber
 	}
 
 	return domain.AdapterResult{}
 }
 
 func (a *adapter) DeleteRules(rules []domain.Rule) (result domain.AdapterResult) {
+	for _, rule := range rules {
+		ruleNumber, keyExists := a.ruleNumbersIndex[rule.String()]
+		if !keyExists {
+			return
+		}
+
+		input := ec2.DeleteNetworkAclEntryInput{
+			Egress:       aws.Bool(rule.Direction.IsOutbound()),
+			NetworkAclId: aws.String(a.NetworkAclId),
+			RuleNumber:   &ruleNumber,
+		}
+
+		req := a.client.DeleteNetworkAclEntryRequest(&input)
+		_, _ = req.Send(context.TODO()) // TODO error handling
+		delete(a.ruleNumbersIndex, rule.String())
+	}
+
 	return domain.AdapterResult{}
 }
