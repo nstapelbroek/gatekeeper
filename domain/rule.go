0feda04594b210aaaddeae0fc850820d4f281f12
Nico Stapelbroek <nstapelbroek@gmail.com>
2020-01-03 22:06:06 +0100

    Use strings.Join to format rules

--- domain/rule.go
+++ domain/rule.go
@@ -1,8 +1,8 @@
 package domain
 
 import (
-	"fmt"
 	"net"
+	"strings"
 )
 
 // Rule is the value object used inside the application to create or delete firewall rules
@@ -14,12 +14,13 @@ type Rule struct {
 }
 
 func (r Rule) String() string {
-	return fmt.Sprintf(
-		"Rule-%s-%s-%s-%s-%s",
-		r.Direction.String(),
-		r.Protocol.String(),
-		r.IPNet.IP.String(),
-		r.IPNet.Mask.String(),
-		r.Port.String(),
-	)
+	return strings.Join(
+		[]string{
+			"Rule",
+			r.Direction.String(),
+			r.Protocol.String(),
+			r.IPNet.IP.String(),
+			r.IPNet.Mask.String(),
+			r.Port.String(),
+		}, "-")
 }
